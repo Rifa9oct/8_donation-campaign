b9f51ee77f4bf5bd59db02ed625909566f8bc418
Rifa9oct <kohinuraktherr@gmail.com>
2023-09-26 01:36:54 +0600

    added sweet alert

--- src/component/DonationDetail/DonationDetail.jsx
+++ src/component/DonationDetail/DonationDetail.jsx
@@ -1,24 +1,33 @@
-import { Link, useLoaderData, useParams } from "react-router-dom";
-
+import { useLoaderData, useParams } from "react-router-dom";
+import Swal from 'sweetalert2'
 const DonationDetail = () => {
     const donations = useLoaderData();
-    const {id} = useParams();
+    const { id } = useParams();
     const idInt = parseInt(id);
     const donation = donations.find(donation => donation.id === idInt);
-    console.log(donation)
 
-    const {img,title,description,price,text_button_bg} = donation;
-     const btnbg ={
-        background:text_button_bg
-     }
-   
+    const { img, title, description, price, text_button_bg } = donation;
+    const btnbg = {
+        background: text_button_bg
+    }
+
+    const handleClickBtn = () => {
+        Swal.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: 'Your donation has been successfully completed',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
+
     return (
         <div className="p-5 lg:p-0">
             <div className="relative">
                 <img className="w-[1320px] lg:h-[700px] rounded-b-lg mx-auto lg:mt-20" src={img} alt="" />
             </div>
             <div className="bg-[#0b0b0b80] w-[386px] md:w-[728px] lg:w-[1320px] h-[80px] md:h-[130px] lg:left-[266px] top-[410px] md:top-[574px] lg:top-[778px] rounded-b-lg mx-auto absolute roundedleft-80">
-                <Link to="/donation"><button style={btnbg} className="text-white py-2 px-2 md:py-4 md:px-[24px] ml-5 md:ml-[37px] mt-5 md:mt-[37px] rounded font-semibold md:text-xl">Donate {price}</button></Link>
+                <button onClick={handleClickBtn} style={btnbg} className="text-white py-2 px-2 md:py-4 md:px-[24px] ml-5 md:ml-[37px] mt-5 md:mt-[37px] rounded font-semibold md:text-xl">Donate {price}</button>
             </div>
             <div className="max-w-[1320px] mx-auto mt-14 mb-[122px]">
                 <h1 className="font-bold text-3xl md:text-[40px] mb-3">{title}</h1>
--- src/component/Header/Navbar.jsx
+++ src/component/Header/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
             <nav className="flex flex-col lg:flex-row justify-between items-center max-w-[1340px] mx-auto mt-14">
                 <img src="/src/assets/logo.png" alt="" />
-                <ul className="flex gap-12  my-10 lg:my-0">
+                <ul className="flex gap-12 text-lg my-10 lg:my-0">
                     <li>
                         <NavLink to='/' className={({ isActive, isPending }) => isActive ? "underline text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Home
                         </NavLink>
