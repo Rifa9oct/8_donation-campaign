51d05a43e1e1288c903c183dd0a5b16cea1ff82e
Rifa9oct <kohinuraktherr@gmail.com>
2023-09-26 11:55:09 +0600

    added README.md file

--- src/component/Header/Navbar.jsx
+++ src/component/Header/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
                 <img src="/src/assets/logo.png" alt="" />
                 <ul className="flex gap-12 text-lg my-10 lg:my-0">
                     <li>
-                        <NavLink to='/' className={({ isActive, isPending }) => isActive ? "underline text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Home
+                        <NavLink to='/' className={({ isActive, isPending }) => isActive ? "underline underline-offset-4 text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Home
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to='/donation' className={({ isActive, isPending }) => isActive ? "underline text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Donation
+                        <NavLink to='/donation' className={({ isActive, isPending }) => isActive ? "underline underline-offset-4 text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Donation
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to='/statistic' className={({ isActive, isPending }) => isActive ? "underline text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Statistic
+                        <NavLink to='/statistic' className={({ isActive, isPending }) => isActive ? "underline underline-offset-4 text-[#FF444A] font-bold" : isPending ? "pending" : ""}>Statistic
                         </NavLink>
                     </li>
                 </ul>
